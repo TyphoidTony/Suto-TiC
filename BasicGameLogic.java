14fca13142f858efb771ac77bd9c6e78efe26886
Anthony <stuthagen@gmail.com>
2015-11-08 19:45:49 -0500

    made the basic methods that each game difficulty needs into an interface

---
+++ BasicGameLogic.java
@@ -0,0 +1,18 @@
+/**
+ * Created on 11/8/2015.
+ * Created by Anthony Soto
+ */
+public interface BasicGameLogic {
+
+
+    void setPos(int x, int y);
+
+    int getX();
+
+    int getY();
+
+    void mkRand();
+
+
+
+}
